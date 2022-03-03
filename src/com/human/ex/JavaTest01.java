738e3f196d68822c3f8f3c5b6b35959cdf7c4a57
foxman12 <foxman12@DESKTOP-V7P9H8V>
2022-03-03 14:26:17 +0900

    first commit

---
+++ src/com/human/ex/JavaTest01.java
@@ -0,0 +1,28 @@
+package com.human.ex;
+
+public class JavaTest01 {
+	/*
+	 * 프로그램의 시작.
+	 * 단축키 설명.
+	 */
+
+	public static void main(String[] args) {
+
+		// ctr + 방향키   단어기준이동
+		// shift + 방향키   영역선택
+		// shift + ctr+방향키   단어기준이동하면서 영역선택
+		// ctr + /      주석 설정 및 해제
+		// home, end  현줄에서 앞부분, 뒷부분 이동
+		// ctr +shift +  + ,-   글자 폰트 크기조절
+		// ctr + f  모두 바꾸기
+		// ctr + z 취소
+		// ctr + y 복구
+		// ctr + c,v,x  복사, 붙여넣기 , 잘라내기
+		
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		
+	}
+
+}
---
+++ src/com/human/ex/JavaTest02.java
@@ -0,0 +1,19 @@
+//37페이지까지 문제 풀고 확인하기
+package com.human.ex;
+
+public class JavaTest02 {
+
+	public static void main(String[] args) {
+		// . 괄호 = , 에서 줄을 변경하여 사용할 수 있다.
+		System.
+			out.
+			println
+			(
+					true
+					)
+			;
+////		Sys
+//		tem.out.println(true);
+	}
+
+}
